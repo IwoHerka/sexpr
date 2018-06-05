dc340272c22934b1afb14aebcf267f12bfdab245
Siegmeyer of Catarina <hi@iwoherka.eu>
2018-06-05 23:32:08 +0200

    Add simple test grammar; move to directory

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,15 @@
-import unittest
+import os
 import sexpr
+import unittest
+import yaml
+
+
+__location__ = os.path.realpath(
+    os.path.join(os.getcwd(), os.path.dirname(__file__))
+)
+
+bool_yml = os.path.join(__location__, 'bool.yml')
+
 
 class Test(unittest.TestCase):
     def test_load_from_object(self):
@@ -11,7 +21,7 @@ class Test(unittest.TestCase):
         self.assertEqual(True, True)
 
     def test_load_from_file(self):
-        sexpr.load('./tests.py')
+        sexpr.load(bool_yml)
         self.assertEqual(True, True)
 
     def test_load_from_dict(self):
