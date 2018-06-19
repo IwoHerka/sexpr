3ed380b7acff70a80669a468d93361a75a463077
Siegmeyer of Catarina <hi@iwoherka.eu>
2018-06-19 02:14:15 +0200

    Add a test for Multiple

--- tests/matching/unit.py
+++ tests/matching/unit.py
@@ -67,7 +67,7 @@ class MatchingUnitTest(unittest.TestCase):
 
     def test_reference(self):
         for _ in range(NR_ROUNDS):
-            grammar = Grammar({})
+            grammar = Grammar({}, dict(root='name'))
             valid, term = choice(terminals)
             grammar.rules['name'] = term
             node = Reference('name', grammar)
@@ -81,6 +81,30 @@ class MatchingUnitTest(unittest.TestCase):
             # Test invalid cannot be popped off
             # (watch out for true-negatives).
             self.assertTrue(node.eat(valid) in [None, 'tring'])
+
+    def test_multiple(self):
+        for _ in range(randint(2, 5)):
+            val, term = choice(terminals)
+            node = Multiple(term, 0, 1)
+            valid = [val]
+
+            for seed in range(10):
+                invalid = random_list(seed=seed)
+                self.assertTrue(node.matches(valid))
+                self.assertTrue(len(node.eat(invalid)) in [len(invalid), len(invalid) - 1])
+                self.assertTrue(node.matches(invalid))
+
+            val, term = choice(terminals)
+            node = Multiple(term, 1, None)
+            valid = [val]
+
+            for seed in range(10):
+                invalid = random_list(seed=seed)
+                mismatched = valid + invalid
+                self.assertTrue(node.matches(valid))
+                self.assertEqual(node.eat(invalid), None)
+                self.assertFalse(node.matches(invalid))
+                self.assertEqual(len(node.eat(mismatched)), len(invalid))
 
     def test_sequence(self):
         for seed in range(NR_ROUNDS):
