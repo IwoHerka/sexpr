b0c3830b47a5d7b637af208cfb6e582a5f62a9cd
Iwo Herka <hi@iwoherka.eu>
2018-11-17 18:18:49 +0100

    Add type-hints to yaml.py

--- sexpr/yaml.py
+++ sexpr/yaml.py
@@ -1,21 +1,26 @@
 import re
+from typing import TYPE_CHECKING, Match, Optional
 
 import yaml
+
+if TYPE_CHECKING:
+    from yamlloader.ordereddict.loaders import Loader
+    from yaml.nodes import ScalarNode
 
 
 class Regexpr(yaml.YAMLObject):
     yaml_tag = u'!regexpr'
 
-    def __init__(self, pattern):
+    @classmethod
+    def from_yaml(cls, loader: 'Loader', node: 'ScalarNode') -> 'Regexpr':
+        return Regexpr(node.value)
+
+    def __init__(self, pattern: str) -> None:
         self.pattern = pattern
         self.re_pattern = re.compile(pattern)
 
-    def matches(self, string):
-        return self.re_pattern.match(string)
-
-    @classmethod
-    def from_yaml(cls, loader, node):
-        return Regexpr(node.value)
-
     def __repr__(self):
         return '(regex %s)' % self.pattern
+
+    def matches(self, string: str) -> Optional[Match[str]]:
+        return self.re_pattern.match(string)
